63394dc27020d7f5a4e44e768613fda124ae5c1e
blue-slushy9 <ra.cruz971@gmail.com>
2024-01-28 17:06:31 -0800

    ok this seems to work now

---
+++ target/debug/deps/rust_hello_world-7a262856133ef9e1.d
@@ -0,0 +1,5 @@
+/Users/aleena/Desktop/GitHub/Rust/rust-hello-world/target/debug/deps/rust_hello_world-7a262856133ef9e1: src/main.rs
+
+/Users/aleena/Desktop/GitHub/Rust/rust-hello-world/target/debug/deps/rust_hello_world-7a262856133ef9e1.d: src/main.rs
+
+src/main.rs:

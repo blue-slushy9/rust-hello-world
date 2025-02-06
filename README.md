da4910138bb45c524ad7c2bb14e92e3718c4a583
blue-slushy9 <107578323+blue-slushy9@users.noreply.github.com>
2025-02-05 18:20:43 -0800

    Cleaned up commits.

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my first Rust program, I read up on this programming language a few days ago and it looks quite interesting!
---
+++ target/debug/rust-hello-world.d
@@ -0,0 +1,1 @@
+/Users/aleena/Desktop/GitHub/Rust/rust-hello-world/target/debug/rust-hello-world: /Users/aleena/Desktop/GitHub/Rust/rust-hello-world/src/main.rs
